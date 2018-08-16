b39905b92b49982ee3d3cc22254824f7594c1cca
Jana Rapava <janarapava@eaton.com>
2018-08-16 15:11:19 +0200

    Problem: Agent names do not correspond to Malamute adresses.
    Solution: Fix this.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

--- include/fty_common_agents.h
+++ include/fty_common_agents.h
@@ -26,30 +26,29 @@
 extern "C" {
 #endif
 
-#define AGENT_FTY_ALERT_ENGINE      "fty-alert-engine"
-#define AGENT_FTY_ALERT_FLEXIBLE    "fty-alert-flexible"
-#define AGENT_FTY_ALERT_LIST        "fty-alert-list"
-#define AGENT_FTY_ASSET             "asset-agent"        // FIXME: to be renamed
-#define AGENT_FTY_DISCOVERY         "fty-discovery"
-#define AGENT_FTY_EMAIL             "fty-email"
-#define AGENT_FTY_INFO              "fty-info"
-#define AGENT_FTY_KPI_POWER_UPTIME  "uptime"             // FIXME: to be renamed
-#define AGENT_FTY_MDNS_SD           "fty-mdns-sd"
-#define AGENT_FTY_METRIC_CACHE      "fty-metric-cache"
+#define AGENT_FTY_ALERT_ENGINE              "fty-alert-engine-server" // FIXME: to be renamed
+#define AGENT_FTY_ALERT_FLEXIBLE            "fty-alert-flexible"
+#define AGENT_FTY_ALERT_LIST                "fty-alert-list"
+#define AGENT_FTY_ASSET                     "asset-agent"        // FIXME: to be renamed
+#define AGENT_FTY_DISCOVERY                 "fty-discovery"
+#define AGENT_FTY_EMAIL                     "fty-email"
+#define AGENT_FTY_EMAIL_SENDMAIL_ONLY       "fty-email-sendmail-only" //FIXME: to be consolidated
+#define AGENT_FTY_INFO                      "fty-info"
+#define AGENT_FTY_KPI_POWER_UPTIME          "uptime"             // FIXME: to be renamed
+#define AGENT_FTY_MDNS_SD                   "fty-mdns-sd"
+#define AGENT_FTY_METRIC_CACHE              "fty-metric-cache"
+// fty-metric-composite has variable number of instances
+#define AGENT_FTY_METRIC_COMPOSITE_PREFIX  "fty-metric-composite"
 
-// FIXME: needs audit
-/* fty-metric-composite:
-#define AGENT_FTY_METRIC_COMPOSITE  "" */
-
-#define AGENT_FTY_METRIC_COMPUTE    "fty-metric-compute"
-#define AGENT_FTY_METRIC_STORE      "fty-metric-store"
+#define AGENT_FTY_METRIC_COMPUTE            "fty-metric-compute"
+#define AGENT_FTY_METRIC_STORE              "fty-metric-store"
 // FIXME: tpower_server actors uses "agent-tpower"!
-#define AGENT_FTY_METRIC_TPOWER     "fty-metric-tpower"
-#define AGENT_FTY_NUT               "fty-nut"
-#define AGENT_FTY_OUTAGE            "fty-outage"
-#define AGENT_FTY_SENSOR_ENV        "fty-sensor-env"
-#define AGENT_FTY_SENSOR_GPIO       "fty-sensor-gpio"
-#define AGENT_FTY_WARRANTY          "warranty-metric"    // FIXME: to be renamed
+#define AGENT_FTY_METRIC_TPOWER             "fty-metric-tpower"
+#define AGENT_FTY_NUT                       "fty-nut"
+#define AGENT_FTY_OUTAGE                    "fty-outage"
+#define AGENT_FTY_SENSOR_ENV                "fty-sensor-env"
+#define AGENT_FTY_SENSOR_GPIO               "fty-sensor-gpio"
+#define AGENT_FTY_WARRANTY                  "warranty-metric"    // FIXME: to be renamed
 
 #ifdef __cplusplus
 }
