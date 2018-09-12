d2c37ebfd7be799c1b3caa66df79ad9d95b69bf7
Jana Rapava <janarapava@eaton.com>
2018-09-12 15:07:55 +0200

    Problem: We need TRANSLATE_ME macro to mark translatable strings.
    Solution: Use variadic macro.
    
    Internet warns that g++ with -pedantic flag may error out in this case, but on our version, it seems to be working.
    
    Signed-off-by: Jana Rapava <janarapava@eaton.com>

---
+++ include/fty_common_macros.h
@@ -0,0 +1,35 @@
+/*  =========================================================================
+    fty_common_macros - System-wide preprocessor macros
+
+    Copyright (C) 2014 - 2018 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+    =========================================================================
+*/
+
+#ifndef FTY_COMMON_MACROS_H_INCLUDED
+#define FTY_COMMON_MACROS_H_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TRANSLATE_ME(...) __VA_ARGS__
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
