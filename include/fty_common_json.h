e34a2978558d48fa5ce600e9b01e7b0bb75158d4
Jiri Kukacka <JiriKukacka@Eaton.com>
2018-11-05 13:08:43 +0100

    Added better description comments to json parser header

--- include/fty_common_json.h
+++ include/fty_common_json.h
@@ -38,14 +38,49 @@ typedef enum {
 
 #ifdef __cplusplus
 namespace JSON {
-    //  @interface
-    // determine start and type of next object in json line
+    /**
+     * \brief Determine start and type of next object in json line
+     * This function tries to determine next JSON type from start_pos based on it's content.
+     * Usage: i=50; getNextObject (line,i); returns next object type after 49th character, and points i to it's start position.
+     * \param[in]       line - JSON fully loaded into string
+     * \param[in,out]   start_pos - location where to start search, on return contains object start position (invalid for non-object results)
+     * \return  JSON_TYPE enum
+     */
     JSON_TYPE getNextObject (const std::string &line, size_t &start_pos);
-    // read string from json
+    /**
+     * \brief Returns string from JSON without validating it
+     * This function reads first string from JSON without validating it, and sets it's start and end position.
+     * Usage: i=50; j; readString (line,i,j); returns next string from JSON after 49th character, and points i to it's start position, and j to it's end.
+     * To get next string, you should do i=j+1; readString (line,i,j);
+     * Beware, first string-like type is returned by this function, so if next type is object that contains string, such string will actually be returned,
+     * so this function traverses JSON tree. You should use getNextObject first to ensure you read proper type.
+     * \param[in]       line - JSON fully loaded into string
+     * \param[in,out]   start_pos - location where to start search, on return contains string start position (invalid for non-object results)
+     * \param[out]      end_pos - on return contains string end position (invalid for non-object results)
+     * \return  JSON_TYPE enum
+     * \throw NotFoundException - in case that no double-quotes encapsulated object is not found
+     * \throw CorruptedLineException - in case that no ending double-quotes are found for the object
+     */
     std::string readString (const std::string &line, size_t &start_pos, size_t &end_pos);
-    // read object from json
+    /**
+     * \brief Returns object from JSON without validating it
+     * This function reads first object from JSON without validating it, and sets it's start and end position.
+     * Usage: i=50; j; readObject (line,i,j); returns next object from JSON after 49th character, and points i to it's start position, and j to it's end.
+     * To get next object, you should do i=j+1; readObject (line,i,j);
+     * Beware, first object-like type is returned by this function, so if next type is string and then next is object, such string will be skipped,
+     * and the object after it will be returned, effectively skipping the string. You should use getNextObject first to ensure you read proper type.
+     * Also be aware that there is no validation, so possibly an object in string might be returned if such object matches requirements.
+     * \param[in]       line - JSON fully loaded into string
+     * \param[in,out]   start_pos - location where to start search, on return contains object start position (invalid for non-object results)
+     * \param[out]      end_pos - on return contains object end position (invalid for non-object results)
+     * \return  JSON_TYPE enum
+     * \throw NotFoundException - in case that no opening curly bracket encapsulated object is not found
+     * \throw CorruptedLineException - in case that no ending curly bracket isn't found for the object
+     */
     std::string readObject (const std::string &line, size_t &start_pos, size_t &end_pos);
+    /// exception that should be used when something is not found
     class NotFoundException {};
+    /// exception that should be used when input line is corrupted somehow
     class CorruptedLineException {};
 }
 #endif
