083bb01982c08adb1ea6c0253f4b4f641e3eeb24
Clément <clementperrette@eaton.com>
2020-11-27 15:25:07 +0000

    Update types/subtypes

--- include/fty_common_asset_types.h
+++ include/fty_common_asset_types.h
@@ -55,6 +55,34 @@ namespace persist {
         NB_ASSET_TYPES
     };
 
+} //namespace persiste
+
+namespace fty {
+    // WARNING keep consistent with DB table t_bios_asset_element_type
+    static constexpr const char* TYPE_UNKNOWN         = "unknown";         // 0
+    static constexpr const char* TYPE_GROUP           = "group";           // 1
+    static constexpr const char* TYPE_DATACENTER      = "datacenter";      // 2
+    static constexpr const char* TYPE_ROOM            = "room";            // 3
+    static constexpr const char* TYPE_ROW             = "row";             // 4
+    static constexpr const char* TYPE_RACK            = "rack";            // 5
+    static constexpr const char* TYPE_DEVICE          = "device";          // 6
+    static constexpr const char* TYPE_INFRA_SERVICE   = "infra-service";   // 7
+    static constexpr const char* TYPE_CLUSTER         = "cluster";         // 8
+    static constexpr const char* TYPE_HYPERVISOR      = "hypervisor";      // 9
+    static constexpr const char* TYPE_VIRTUAL_MACHINE = "virtual-machine"; // 10
+    static constexpr const char* TYPE_STORAGE_SERVICE = "storage-service"; // 11
+    static constexpr const char* TYPE_VAPP            = "vapp";            // 12
+    static constexpr const char* TYPE_CONNECTOR       = "connector";       // 13
+    static constexpr const char* TYPE_SERVER          = "server";          // 15
+    static constexpr const char* TYPE_PLANNER         = "planner";         // 16
+    static constexpr const char* TYPE_PLAN            = "plan";            // 17
+    //18
+    //19
+    static constexpr const char* TYPE_COPS            = "cops";            // 20
+
+} //namespace fty
+
+namespace persist {
     enum asset_subtype {
         SUNKNOWN                        = 0,
 
@@ -129,7 +157,75 @@ namespace persist {
 
         NB_ASSET_SUBTYPES
     };
+}
 
+namespace fty {
+ // WARNING keep consistent with DB table t_bios_asset_device_type
+    static constexpr const char* SUB_UNKNOWN                          = "unknown";                          // 0
+    static constexpr const char* SUB_UPS                              = "ups";                              // 1
+    static constexpr const char* SUB_GENSET                           = "genset";                           // 2
+    static constexpr const char* SUB_EPDU                             = "epdu";                             // 3
+    static constexpr const char* SUB_PDU                              = "pdu";                              // 4
+    static constexpr const char* SUB_SERVER                           = "server";                           // 5
+    static constexpr const char* SUB_FEED                             = "feed";                             // 6
+    static constexpr const char* SUB_STS                              = "sts";                              // 7
+    static constexpr const char* SUB_SWITCH                           = "switch";                           // 8
+    static constexpr const char* SUB_STORAGE                          = "storage";                          // 9
+    static constexpr const char* SUB_VM                               = "vm";                               // 10
+    static constexpr const char* SUB_N_A                              = "N_A";                              // 11
+    static constexpr const char* SUB_ROUTER                           = "router";                           // 12
+    static constexpr const char* SUB_RACK_CONTROLLER                  = "rack controller";                  // 13
+    static constexpr const char* SUB_SENSOR                           = "sensor";                           // 14
+    static constexpr const char* SUB_APPLIANCE                        = "appliance";                        // 15
+    static constexpr const char* SUB_CHASSIS                          = "chassis";                          // 16
+    static constexpr const char* SUB_PATCH_PANEL                      = "patch panel";                      // 17
+    static constexpr const char* SUB_OTHER                            = "other";                            // 18
+    static constexpr const char* SUB_SENSORGPIO                       = "sensorgpio";                       // 19
+    static constexpr const char* SUB_GPO                              = "gpo";                              // 20
+    static constexpr const char* SUB_NETAPP_ONTAP_NODE                = "netapp.ontap.node";                // 21
+    static constexpr const char* SUB_IPMINFRA_SERVER                  = "ipminfra.server";                  // 22
+    static constexpr const char* SUB_IPMINFRA_SERVICE                 = "ipminfra.service";                 // 23
+    static constexpr const char* SUB_VMWARE_VCENTER                   = "vmware.vcenter";                   // 24
+    static constexpr const char* SUB_CITRIX_POOL                      = "citrix.pool";                      // 25
+    static constexpr const char* SUB_VMWARE_CLUSTER                   = "vmware.cluster";                   // 26
+    static constexpr const char* SUB_VMWARE_ESXI                      = "vmware.esxi";                      // 27
+    static constexpr const char* SUB_MICROSOFT_HYPERV                 = "microsoft.hyperv";                 // 28
+    static constexpr const char* SUB_VMWARE_VM                        = "vmware.vm";                        // 29
+    static constexpr const char* SUB_MICROSOFT_VM                     = "microsoft.vm";                     // 30
+    static constexpr const char* SUB_CITRIX_VM                        = "citrix.vm";                        // 31
+    static constexpr const char* SUB_NETAPP_NODE                      = "netapp.node";                      // 32
+    static constexpr const char* SUB_VMWARE_STANDALONE_ESXI           = "vmware.standalone.esxi";           // 33
+    static constexpr const char* SUB_VMWARE_TASK                      = "vmware.task";                      // 34
+    static constexpr const char* SUB_VMWARE_VAPP                      = "vmware.vapp";                      // 35
+    static constexpr const char* SUB_CITRIX_XENSERVER                 = "citrix.xenserver";                 // 36
+    static constexpr const char* SUB_CITRIX_VAPP                      = "citrix.vapp";                      // 37
+    static constexpr const char* SUB_CITRIX_TASK                      = "citrix.task";                      // 38
+    static constexpr const char* SUB_MICROSOFT_VIRTUALIZATION_MACHINE = "microsoft.virtualization.machine"; // 39
+    static constexpr const char* SUB_MICROSOFT_TASK                   = "microsoft.task";                   // 40
+    static constexpr const char* SUB_MICROSOFT_SERVER_CONNECTOR       = "microsoft.server.connector";       // 41
+    static constexpr const char* SUB_MICROSOFT_SERVER                 = "microsoft.server";                 // 42
+    static constexpr const char* SUB_MICROSOFT_CLUSTER                = "microsoft.cluster";                // 43
+    static constexpr const char* SUB_HP_ONEVIEW_CONNECTOR             = "hp.oneview.connector";             // 44
+    static constexpr const char* SUB_HP_ONEVIEW                       = "hp.oneview";                       // 45
+    static constexpr const char* SUB_HP_IT_SERVER                     = "hp.it.server";                     // 46
+    static constexpr const char* SUB_HP_IT_RACK                       = "hp.it.rack";                       // 47
+    static constexpr const char* SUB_NETAPP_SERVER                    = "netapp.server";                    // 48
+    static constexpr const char* SUB_NETAPP_ONTAP_CONNECTOR           = "netapp.ontap.connector";           // 49
+    static constexpr const char* SUB_NETAPP_ONTAP_CLUSTER             = "netapp.ontap.cluster";             // 50
+    static constexpr const char* SUB_NUTANIX_VM                       = "nutanix.vm";                       // 51
+    static constexpr const char* SUB_NUTANIX_PRISM_GATEWAY            = "nutanix.prism.gateway";            // 52
+    static constexpr const char* SUB_NUTANIX_NODE                     = "nutanix.node";                     // 53
+    static constexpr const char* SUB_NUTANIX_CLUSTER                  = "nutanix.cluster";                  // 54
+    static constexpr const char* SUB_NUTANIX_PRISM_CONNECTOR          = "nutanix.prism.connector";          // 55
+    static constexpr const char* SUB_VMWARE_VCENTER_CONNECTOR         = "vmware.vcenter.connector";         // 60
+    static constexpr const char* SUB_VMWARE_STANDALONE_ESXI_CONNECTOR = "vmware.standalone.esxi.connector"; // 61
+    static constexpr const char* SUB_NETAPP_ONTAP                     = "netapp.ontap";                     // 62
+    static constexpr const char* SUB_VMWARE_SRM                       = "vmware.srm";                       // 65
+    static constexpr const char* SUB_VMWARE_SRM_PLAN                  = "vmware.srm.plan";                  // 66
+    static constexpr const char* SUB_PCU                              = "pcu";                              // 72
+} //namespace fty
+
+namespace persiste {
     enum asset_operation
     {
         INSERT = 1,
@@ -174,7 +270,7 @@ namespace persist {
     is_ups(int x);
 
     bool
-    is_container (std::string asset_type);
+    is_container (const std::string & asset_type);
 
     bool
     is_ok_element_type (uint16_t element_type_id);
